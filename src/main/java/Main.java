5b1fbb7c9319eb396648c0d6b837a972d91e1273
Ibne Nahian <evilprince2009@gmail.com>
2022-01-22 15:25:35 +0600

    QR code generator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    }
+
+    private static void generateQRCode(String data, String path, int dimension) throws Exception {
+        BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes("UTF-8"), "UTF-8"), BarcodeFormat.QR_CODE, dimension, dimension);
+        MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
     }
 }
