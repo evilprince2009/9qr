d2c04b3f1c29ac9b98fc3b032f20320231c4a84f
Ibne Nahian <evilprince2009@gmail.com>
2022-01-22 15:29:12 +0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,15 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-
+        String data = "https://www.github.com/evilprince2009";
+        String path = "D:\\Dump\\link-to-gh.png";
+        int dimension = 200;
+        try {
+            generateQRCode(data, path, dimension);
+            System.out.println("QR Code created successfully. Please check the file at " + path);
+        } catch (Exception ex) {
+            System.out.println("QR Code creation failed: " + ex.getMessage());
+        }
     }
 
     private static void generateQRCode(String data, String path, int dimension) throws Exception {
