9060e95c16a1cb017b262ba3977ac307ef2863ba
Ibne Nahian <evilprince2009@gmail.com>
2022-06-22 21:18:44 +0600

    upgrade

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,26 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import java.io.File;
+import java.util.Scanner;
 
 public class Main {
+    private static final Scanner buffer = new Scanner(System.in);
     public static void main(String[] args) {
-        String data = "https://www.github.com/evilprince2009";
-        String path = ".\\images\\link-to-gh.png";
+        System.out.print("Input Data: ");
+        String data = buffer.next();
+        System.out.println();
+        System.out.print("Save file path: ");
+        String path = buffer.next();
+        System.out.println();
+        System.out.print("Filename: ");
+        String filename = buffer.next();
+
+        buffer.close();
+
+        String fullDir = path + "\\" + filename + ".png";
         int dimension = 200;
         try {
-            generateQRCode(data, path, dimension);
+            generateQRCode(data, fullDir, dimension);
             System.out.println("QR Code created successfully. Please check the file at " + path);
         } catch (Exception ex) {
             System.out.println("QR Code creation failed: " + ex.getMessage());
